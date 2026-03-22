c8fff2da1740dee3b8e58be2e489e9f834be71c9
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-03-22 16:32:53 +0530

    Added Leetcode

---
+++ src/main/java/BackTrack/BackTracking1.java
@@ -0,0 +1,74 @@
+package BackTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//import static BackTrack.BackTracking1.BackTrack;
+
+
+// This is the Code of leetcode 46 solutions of permutations.
+
+public class BackTracking1 {
+    public static void main(String[] args) {
+        int n;
+        List<List<Integer>> result = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        n=sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++){
+            System.out.print("Enter the "+(i+1)+"th element : ");
+            arr[i] = sc.nextInt();
+        }
+        for (int i : arr){
+            System.out.print(i+" ");
+        }
+
+        result = permute(arr,n);
+        System.out.println(result);
+
+    }
+
+    public static List<List<Integer>> permute(int arr[], int n){
+        List<List<Integer>> result = new ArrayList<>();
+        BackTrack(arr,result,new ArrayList<>(),n);
+
+        return result;
+    }
+    public static void BackTrack(int[] arr, List<List<Integer>> result, List<Integer> current, int n){
+        if (current.size()== n){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i =0;i<n;i++){
+            if (current.contains(arr[i])){
+                continue;
+            }
+            current.add(arr[i]);
+            BackTrack(arr,result,current,n);
+            current.remove(current.size()-1);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
