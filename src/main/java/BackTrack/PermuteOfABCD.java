c691587b49e364d7a0efa5efce57e7c7afe51cdd
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-04 12:08:42 +0530

    an incomplete code for now

---
+++ src/main/java/BackTrack/PermuteOfABCD.java
@@ -0,0 +1,24 @@
+package BackTrack;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PermuteOfABCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String of which you want permutations : ");
+        String s = sc.nextLine();
+        List<List<Character>> list = new ArrayList<>();
+        list =permute(s,s.length(),new ArrayList<>());
+    }
+
+    public static List<List<Character>> permute(String s,int n,List<Character> current){
+        if(current.size()==n){
+// will complete it soon
+        }
+        return
+    }
+}
