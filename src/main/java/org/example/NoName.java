ca60ea67e0505c8c0cdb87dfaed8fd9dbf2accaa
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-15 16:01:52 +0530

    added NoName Java File

---
+++ src/main/java/org/example/NoName.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NoName {
+    public static void main(String[] args) {
+         String s;
+         Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        s=sc.nextLine();
+        System.out.println("Hello !! "+s);
+    }
+}
