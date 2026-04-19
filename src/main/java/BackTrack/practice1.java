58e02297f8f7f1802b4946aff1edca8d160988de
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-19 23:08:31 +0530

    nothing

--- src/main/java/BackTrack/practice1.java
+++ src/main/java/BackTrack/practice1.java
@@ -4,7 +4,10 @@ public class practice1 {
 
     public static void printPermutation(String str , String perm, int idx){
 
+            for (int i = 0 ;i<str.length();i++){
 
+
+            }
     }
     public static void main(String[] args) {
         String str = "ABC";
