18f70832f61bd3daa6daa4f45b0904aad68fac4c
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-19 23:07:22 +0530

    starting practice

---
+++ src/main/java/BackTrack/practice1.java
@@ -0,0 +1,12 @@
+package BackTrack;
+
+public class practice1 {
+
+    public static void printPermutation(String str , String perm, int idx){
+
+
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+    }
+}
